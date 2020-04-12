855756a4f8af5f7490b9c53db9a015372df1a88e
gabrielvaraljay <gabriel.varaljay@gmail.com>
2020-04-12 12:35:47 +0100

    logo change

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $keywords = json_decode(file_get_contents("config/keywords.json"), TRUE);
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light bg-transparent fixed-top">
-        <a class="navbar-brand"><img src="img/envienta_platform_logo.png" /></a>
+        <a class="navbar-brand"><img src="img/envienta_logo.png" /></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
