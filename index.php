0a00a4e398dbf7744a22096fa0582ee556c74dbd
gabrielvaraljay <gabriel.varaljay@gmail.com>
2020-04-14 22:36:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -115,7 +115,7 @@ $keywords = json_decode(file_get_contents("config/keywords.json"), TRUE);
     </main>
 
     <div class="footer">
-        <p>ENVIENTA @2020 <span data-localize="allrights”>All Rights reserved.</span> | <span data-localize="background”>Background:</span>
+        <p>ENVIENTA @2020 <span data-localize="allrights">All Rights reserved.</span> | <span data-localize="background">Background:</span>
             <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, San Francisco, Helvetica Neue, Helvetica, Ubuntu, Roboto, Noto, Segoe UI, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@federicorespini?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Federico Respini"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32">
                         <title>unsplash-logo</title>
                         <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
