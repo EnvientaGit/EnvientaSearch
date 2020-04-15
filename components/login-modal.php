115f663ae66cec72a0185a2f6555acf107d8b41a
gabrielvaraljay <gabriel.varaljay@gmail.com>
2020-04-15 22:03:37 +0100

    Update login-modal.php
    localization

--- components/login-modal.php
+++ components/login-modal.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Sign Up / Sign In</h5>
+                <h5 class="modal-title"><span data-localize="signup">Sign Up / Sign In</span></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
