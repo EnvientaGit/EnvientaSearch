f0b1e041c9c1f1adaed447785e9670fa72506e40
gabrielvaraljay <gabriel.varaljay@gmail.com>
2020-04-15 21:27:17 +0100

    map-target-modal localization

--- components/map-target-modal.php
+++ components/map-target-modal.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Select your a location</h5>
+                <h5 class="modal-title"><span data-localize="selectlocation">Select your a location.</span></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -10,7 +10,10 @@
             <form id="mapTargetForm">
                 <div class="modal-body">
                     <div class="form-group">
-                        <small id="mapTargetAddressHelp" class="form-text text-muted">Start typing your address, then press ENTER to position the marker on the map. You can refine it by moving the marker. Don’t worry we don’t store your location, but that’s how the search engine works. <strong>Entering the address is important so that the search engine can only list the results that are closest to you.</strong></small>
+                        <h5> <small id="mapTargetAddressHelp" class="form-text text-muted"><span data-localize="typeaddress">Start typing your address, then press ENTER to position the marker on the map. You can refine it by moving the marker. Don’t worry we don’t store your location, but that’s how the search engine works.</span> 
+                        <br />
+                        <br />
+                        <strong> <span data-localize="enteringaddress">Entering the address is important so that the search engine can only list the results that are closest to you. </span> </strong></small></h5>
                     </div>
                     <div class="form-group">
                         <input type="text" class="form-control" id="mapTargetAddress" aria-describedby="mapTargetAddressHelp">
@@ -18,8 +21,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button id="mapTargetSaveButton" type="button" class="btn btn-primary">Save</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal"><span data-localize="cancel">Cancel</span></button>
+                    <button id="mapTargetSaveButton" type="button" class="btn btn-primary"><span data-localize="save">Save</span></button>
                 </div>
             </form>
         </div>
