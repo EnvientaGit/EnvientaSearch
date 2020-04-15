cddb0a257e43334b69c74437ba00eeecc05407c0
gabrielvaraljay <gabriel.varaljay@gmail.com>
2020-04-15 21:59:40 +0100

    whoweare-modal localization

--- components/whoweare-modal.php
+++ components/whoweare-modal.php
@@ -2,13 +2,13 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Who we are?!</h5>
+                <h5 class="modal-title"><span data-localize="whoweare">Who we are?</span></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-            The ENVIENTA team is working to make product development open-source and localize production. The same goes for local products. Buy local products to help local growers and manufacturers. This website serves this purpose.
+            <span data-localize="whowearetext">The ENVIENTA team is working to make product development open-source and localize production. The same goes for local products. Buy local products to help local growers and manufacturers. This website serves this purpose.</span>
             </div>
         </div>
     </div>
