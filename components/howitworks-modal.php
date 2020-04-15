d07e1e89f4e0ae791855ba4e8124ec5ea0e2b1a9
gabrielvaraljay <gabriel.varaljay@gmail.com>
2020-04-15 21:56:11 +0100

    howitworks-modal localization

--- components/howitworks-modal.php
+++ components/howitworks-modal.php
@@ -8,7 +8,7 @@
                 </button>
             </div>
             <div class="modal-body">
-            This page is a local search engine. It can be a product, service or volunteer help you will find everything here. All you have to do is enter your location and type in the keyword you are looking for. We are already working on a registration module, so if you have a product, service or you want to support your community, you can be added yourself to this site soon.
+            <span data-localize="thispage">This page is a local search engine. It can be a product, service or volunteer help you will find everything here. All you have to do is enter your location and type in the keyword you are looking for. We are already working on a registration module, so if you have a product, service or you want to support your community, you can be added yourself to this site soon.</span>
             </div>
         </div>
     </div>
